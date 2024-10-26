71251ea0e95e081ee371d79298e59c815ef08ee0
Eugene Baranovsky <baranovsky.e@gmail.com>
2024-10-27 01:32:18 +1300

    16 current  2024-10-27 01:25:05  24.05.5919.32e940c7c420  6.11.4-xanmod1                          *

--- configuration.nix
+++ configuration.nix
@@ -26,6 +26,12 @@
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
+  # Bluetooth
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+  };
+
   # Enable networking
   networking.networkmanager.enable = true;
 
