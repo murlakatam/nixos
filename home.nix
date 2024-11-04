11b603c15f502b2189ce434cf2fb151bd4314206
Eugene Baranovsky <baranovsky.e@gmail.com>
2024-11-04 20:03:01 +1300

    26 current  2024-11-04 20:02:58  24.11.20241102.7ffd9ae   6.11.5-xanmod1                          *

--- home.nix
+++ home.nix
@@ -16,6 +16,14 @@
     oh-my-zsh
   ];
 
+  programs.firefox = {
+    enable = true;
+    languagePacks = [
+      "en-GB"
+      "be"
+    ];
+  };
+
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new Home Manager release introduces backwards
