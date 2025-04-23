0c9429f25a39d4bbae2adef5a4598d67154e67f3
murlakatam <baranovsky.e@gmail.com>
2025-04-23 21:38:32 +1200

    112 current  2025-04-23 21:03:22  25.05.20250421.c11863f  6.14.1-zen1                          *

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "My flake NixOs configuration";
 
   inputs = {
-    nixpkgs.url = "git+https://github.com/NixOS/nixpkgs?shallow=1&ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
