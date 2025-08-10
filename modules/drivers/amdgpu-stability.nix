6d552055282f4ff58a81d383c9bd59c6208885f3
murlakatam <baranovsky.e@gmail.com>
2025-08-11 02:33:30 +1200

    141 2025-08-11 00:32:09 25.11.20250809.85dbfc7 6.15.8-zen1 Unknown []

--- modules/drivers/amdgpu-stability.nix
+++ modules/drivers/amdgpu-stability.nix
@@ -15,12 +15,8 @@ in {
     # Apply the hardcoded patches from local files
     # boot.kernelPatches = [
     #   {
-    #     name = "amdgpu-vcn-reset-fix-1";
-    #     patch = ./amdgpu-vcn-reset-fix-1.patch;
-    #   }
-    #   {
-    #     name = "amdgpu-vcn-reset-fix-2";
-    #     patch = ./amdgpu-vcn-reset-fix-2.patch;
+    #     name = "amdgpu-vrr-fix";
+    #     patch = ./amdgpu-vrr-fix.patch;
     #   }
     # ];
   };
