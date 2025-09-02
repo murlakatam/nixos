2420de17b41f64f4db73343d2111ac79dbc29e59
murlakatam <baranovsky.e@gmail.com>
2025-09-02 21:42:33 +1200

    211 2025-09-02 21:42:30 25.11.20250814.fbcf476 6.16.0-zen1 Unknown []

--- modules/homeManagerModules/lazysql-config.nix
+++ modules/homeManagerModules/lazysql-config.nix
@@ -4,47 +4,31 @@
   pkgs,
   ...
 }: let
-  # We will give our connection a service name.
-  pgServiceName = "moe_devtest_reader_service";
-
-  # Your connection details, written plainly.
+  # Define your connection details here for clarity
   dbHost = "mataersdevtestfpsqlserver.postgres.database.azure.com";
   dbName = "postgres";
-  dbUsername = "AL PSQL ERS DEVTEST READER";
+  # The username, still URL-encoded for use in the query string
+  dbUsernameEncoded = "AL%20PSQL%20ERS%20DEVTEST%20READER";
 
-  # Path to your zsh functions file.
+  # Set the correct path to the file containing your Zsh functions.
   zshFunctionsFile = "${config.home.homeDirectory}/.zshrc";
 in {
-  # --- Part 1: Create the PostgreSQL Service File ---
-  # This file defines connection parameters for a named service.
-  # The database driver will read this file automatically.
-  # It is typically located at ~/.pg_service.conf
-  home.file.".pg_service.conf".text = ''
-    [${pgServiceName}]
-    host=${dbHost}
-    dbname=${dbName}
-    user=${dbUsername}
-  '';
-
-  # --- Part 2: Configure lazysql to use the Service ---
   xdg.configFile."lazysql/config.toml".text = ''
+    # Database connection block for your Azure DB
     [[database]]
-    Name = "Azure DevTest DB Reader"
+    Name = "Azure DevTest DB"
     Provider = "postgres"
     DBName = "${dbName}"
-
-    # --- THE DEFINITIVE FIX ---
-    # The URL now simply refers to the service name defined above.
-    # This delegates all connection parameter handling to the robust pg driver,
-    # completely avoiding the string parsing bug.
-    URL = "postgresql://?service=${pgServiceName}"
-
-    # Pre-connection commands remain the same.
+    # Use the key-value DSN format instead of a URL
+    URL = "postgres://${dbUsernameEncoded}@${dbHost}/${dbName}"
+    #
+    # Commands to run BEFORE connecting
     Commands = [
       { Command = "zsh -c 'source ${zshFunctionsFile}; allow-me-2-postgres'" },
       { Command = "zsh -c 'source ${zshFunctionsFile}; put-azure-token-in-pgpass'" }
     ]
 
+    # General application settings (optional)
     [application]
     DefaultPageSize = 300
   '';
