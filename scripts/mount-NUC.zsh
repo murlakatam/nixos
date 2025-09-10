b41a1b8d2a9c0de8827840b51f93c1d7a1aad238
murlakatam <baranovsky.e@gmail.com>
2025-09-10 22:07:10 +1200

    NixOS Gen 253 (25.11.20250902.d0fc308)

--- scripts/mount-NUC.zsh
+++ scripts/mount-NUC.zsh
@@ -1,5 +1,6 @@
 function mount-NUC() {
     fusermount -u ~/mnt/NUC
-    sshfs eugene@192.168.0.52:/mnt/MediaLibrary4TB ~/mnt/NUC -o reconnect,compression=yes,ServerAliveInterval=15,ServerAliveCountMax=3
-
+    sudo fusermount -u /mnt/NUC
+    sudo mkdir -p /mnt/NUC
+    sudo sshfs eugene@192.168.0.52:/mnt/MediaLibrary4TB /mnt/NUC -o reconnect,compression=yes,ServerAliveInterval=15,ServerAliveCountMax=3
 }
