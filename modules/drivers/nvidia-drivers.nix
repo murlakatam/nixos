d8cb329ada819b64dae4ae6bb59cd2ee59aefe66
murlakatam <baranovsky.e@gmail.com>
2025-09-19 16:56:03 +1200

    NixOS Gen 259 (25.11.20250913.c23193b)

--- modules/drivers/nvidia-drivers.nix
+++ modules/drivers/nvidia-drivers.nix
@@ -13,26 +13,45 @@ in {
   config = mkMerge [
     # Configuration when NVIDIA is enabled
     (mkIf cfg.enable {
+      # Enable OpenGL
+      hardware.graphics = {
+        enable = true;
+      };
+
+      # Load nvidia driver for Xorg and Wayland
       services.xserver.videoDrivers = ["nvidia"];
+
       hardware.nvidia = {
         # Modesetting is required.
         modesetting.enable = true;
+
         # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+        # Enable this if you have graphical corruption issues or application crashes after waking
+        # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
+        # of just the bare essentials.
         powerManagement.enable = false;
+
         # Fine-grained power management. Turns off GPU when not in use.
         # Experimental and only works on modern Nvidia GPUs (Turing or newer).
         powerManagement.finegrained = false;
+
         # Use the NVidia open source kernel module (not to be confused with the
         # independent third-party "nouveau" open source driver).
         # Support is limited to the Turing and later architectures. Full list of
         # supported GPUs is at:
         # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
         # Only available from driver 515.43.04+
-        # Currently alpha-quality/buggy, so false is currently the recommended setting.
-        open = false;
+        # An important note to take is that the option hardware.nvidia.open
+        # should only be set to false if you have a GPU with an older architecture than Turing (older than the RTX 20-Series).
+        # For newer cards it is recommended by NVIDIA to use the open Drivers:
+        # https://download.nvidia.com/XFree86/Linux-x86_64/565.77/README/kernel_open.html
+        # So If you have an GPU with Turing architecture (RTX 20-Series) or newer set hardware.nvidia.open to true
+        open = true;
+
         # Enable the Nvidia settings menu,
         # accessible via `nvidia-settings`.
         nvidiaSettings = true;
+
         # Optionally, you may need to select the appropriate driver version for your specific GPU.
         package = config.boot.kernelPackages.nvidiaPackages.stable;
       };
