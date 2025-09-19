4979edf19366c860458f2583f0f58d549a88cb86
murlakatam <baranovsky.e@gmail.com>
2025-09-19 21:36:41 +1200

    NixOS Gen 262 (25.11.20250913.c23193b)

--- modules/nixosModules/programs.nix
+++ modules/nixosModules/programs.nix
@@ -3,5 +3,11 @@
   programs = {
     # allow managing GNOME settings through Home Manager
     dconf.enable = true;
+    steam = {
+      enable = true;
+      remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+      dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+      localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+    };
   };
 }
