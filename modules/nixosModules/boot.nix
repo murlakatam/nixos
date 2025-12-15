d9835c85cc250748aaedfcc01b62923ef85194e3
murlakatam <baranovsky.e@gmail.com>
2025-12-16 08:55:36 +1100

    NixOS Gen 287 (26.05.20251211.2fbfb1d)

--- modules/nixosModules/boot.nix
+++ modules/nixosModules/boot.nix
@@ -7,7 +7,7 @@
     #linuxPackages_latest
     #linuxPackages_xanmod_latest
     #linuxPackages_zen
-    kernelPackages = pkgs.linuxPackages_zen;
+    kernelPackages = pkgs.linuxPackages_xanmod_latest;
     loader = {
       efi.canTouchEfiVariables = true;
       systemd-boot = {
