04debc9a72eecbf7d2111bfc3d137636e72a0aeb
murlakatam <baranovsky.e@gmail.com>
2025-12-26 13:46:36 +1100

    NixOS Gen 288 (26.05.20251211.2fbfb1d)

--- hosts/proartp16/home.nix
+++ hosts/proartp16/home.nix
@@ -116,12 +116,12 @@ in {
   services.udiskie = {
     enable = true;
     settings = {
-        # workaround for
-        # https://github.com/nix-community/home-manager/issues/632
-        program_options = {
-            # replace with your favorite file manager
-            file_manager = "${terminalExe} -e ${pkgs.yazi}/bin/yazi";
-        };
+      # workaround for
+      # https://github.com/nix-community/home-manager/issues/632
+      program_options = {
+        # replace with your favorite file manager
+        file_manager = "${terminalExe} -e ${pkgs.yazi}/bin/yazi";
+      };
     };
-};
+  };
 }
