e4529c93dba9faac654c5a8cbf6ab91d257e5d9e
murlakatam <baranovsky.e@gmail.com>
2026-01-18 02:59:37 +1100

    NixOS Gen 326 (26.05.20260116.e4bae1b)

--- modules/nixosModules/users.nix
+++ modules/nixosModules/users.nix
@@ -22,6 +22,12 @@ in {
           #"libvirtd" manage virutal maching
           # "scanner" scanning
           #"lp" printing
+          "docker" # for dorker
+          "video" # Grants permission to access video hardware like webcams and, crucially, graphics cards.
+          "audio" # Grants direct access to sound hardware.
+          "input" # Grants permission to read from input devices
+          "uinput" # Grants permission to create virtual input devices.
+          "i2c" # Grants permission to access the I2C bus (Inter-Integrated Circuit), a communication protocol used for internal hardware components.
         ];
         shell = pkgs.zsh;
         ignoreShellProgramCheck = true;
