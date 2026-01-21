67f2c3c31ce26345f08707d0f3e17dbeb6f28468
murlakatam <baranovsky.e@gmail.com>
2026-01-21 16:00:15 +1100

    NixOS Gen 362 (26.05.20260116.e4bae1b)

--- modules/nixosModules/gnome-extensions.nix
+++ modules/nixosModules/gnome-extensions.nix
@@ -1,65 +1,81 @@
-{ config, pkgs, lib, ... }:
-
-let
-  # Define the extension package once to ensure paths match exactly
-  batteryExtension = pkgs.gnomeExtensions.battery-health-charging;
-  
-  # The helper script path inside the extension
-  # Note: The UUID must match exactly what is in the /share path of the package
-  extensionUuid = "battery-health-charging@maniacx.github.com";
-  executablePath = "${batteryExtension}/share/gnome-shell/extensions/${extensionUuid}/resources/batteryhealthchargingctl";
-
-  # Define the Polkit Policy strictly matching the installer's XML template
-  batteryHealthPolkit = pkgs.writeTextFile {
-    name = "battery-health-charging-polkit";
-    destination = "/share/polkit-1/actions/org.freedesktop.policykit.batteryhealthcharging.setthreshold.policy";
-    text = ''
-      <?xml version="1.0" encoding="UTF-8"?>
-      <!DOCTYPE policyconfig PUBLIC "-//freedesktop//DTD PolicyKit Policy Configuration 1.0//EN" "http://www.freedesktop.org/standards/PolicyKit/1/policyconfig.dtd">
-      <policyconfig>
-        <vendor>Battery Health Charging</vendor>
-        <vendor_url>https://github.com/maniacx/Battery-Health-Charging</vendor_url>
-        
-        <action id="org.freedesktop.policykit.batteryhealthcharging.setthreshold">
-          <description>Control battery thresholds</description>
-          <message>No Authorization required to control battery thresholds.</message>
-          <defaults>
-            <allow_any>yes</allow_any>
-            <allow_inactive>yes</allow_inactive>
-            <allow_active>yes</allow_active>
-          </defaults>
-          
-          <annotate key="org.freedesktop.policykit.exec.path">${executablePath}</annotate>
-          
-          <annotate key="org.freedesktop.policykit.batteryhealthcharging.setthreshold.polkit-rule.version">3.0.0</annotate>
-        </action>
-      </policyconfig>
-    '';
-  };
-
-in
 {
-  # 1. Install Extensions + The Polkit Rule
+  config,
+  pkgs,
+  lib,
+  ...
+}: let
+  # 1. Define the specific extension
+  baseExtension = pkgs.gnomeExtensions.battery-health-charging;
+  uuid = "battery-health-charging@maniacx.github.com";
+
+  # 2. Extract the 'ctl' script ROBUSTLY
+  # We use 'find' to locate the script because the directory structure
+  # can vary between Nixpkgs versions.
+  batteryCtlTool = pkgs.runCommand "battery-health-ctl" {} ''
+    mkdir -p $out/bin
+
+    # Find the file anywhere inside the extension directory
+    SCRIPT_PATH=$(find ${baseExtension} -name batteryhealthchargingctl -type f | head -n 1)
+
+    if [ -z "$SCRIPT_PATH" ]; then
+      echo "ERROR: Could not find batteryhealthchargingctl in ${baseExtension}"
+      exit 1
+    fi
+
+    cp "$SCRIPT_PATH" $out/bin/batteryhealthchargingctl
+    chmod +x $out/bin/batteryhealthchargingctl
+  '';
+
+  # 3. Patch the extension to use the global system path
+  patchedExtension = baseExtension.overrideAttrs (old: {
+    postPatch =
+      (old.postPatch or "")
+      + ''
+        # We substitute the path in driver.js to point to our extracted tool
+        substituteInPlace lib/driver.js \
+          --replace-fail '/usr/local/bin/batteryhealthchargingctl-''${user}' \
+                         '/run/current-system/sw/bin/batteryhealthchargingctl'
+      '';
+  });
+in {
+  # Install the patched extension AND the standalone tool
   environment.systemPackages = with pkgs; [
-    batteryExtension
+    patchedExtension
+    batteryCtlTool
     gnomeExtensions.window-calls
-    batteryHealthPolkit
-    # Add other extensions here
   ];
 
-  # 2. Enable extensions by default (Dconf)
-  programs.dconf.profiles.user.databases = [{
-    settings = {
-      "org/gnome/shell" = {
-        disable-user-extensions = false;
-        enabled-extensions = [
-          extensionUuid
-          "window-calls@swyknox.github.com"
-        ];
-      };
-    };
-  }];
+  # 4. Polkit Rule (Javascript)
+  # Allows the binary at /run/current-system/sw/bin/... to run without sudo password
+  security.polkit.extraConfig = ''
+    polkit.addRule(function(action, subject) {
+      if (action.id == "org.freedesktop.policykit.exec" &&
+          action.lookup("program") == "/run/current-system/sw/bin/batteryhealthchargingctl" &&
+          subject.local && subject.isInGroup("wheel"))
+      {
+        return polkit.Result.YES;
+      }
+    });
+  '';
 
-  # 3. Ensure Polkit is enabled
+  # 5. Dconf Settings to enable it automatically
+  programs.dconf.profiles.user.databases = [
+    {
+      settings = {
+        "org/gnome/shell" = {
+          disable-user-extensions = false;
+          enabled-extensions = [
+            uuid
+            "window-calls@swyknox.github.com"
+          ];
+        };
+        # Force "installed" status to suppress the UI error popup
+        "org/gnome/shell/extensions/Battery-Health-Charging" = {
+          polkit-status = "installed";
+        };
+      };
+    }
+  ];
+
   security.polkit.enable = true;
 }
