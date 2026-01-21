c6b7d13a1fab79cb5c1cd0fd60736d543bd8299f
murlakatam <baranovsky.e@gmail.com>
2026-01-21 15:47:17 +1100

    NixOS Gen 361 (26.05.20260116.e4bae1b)

--- modules/nixosModules/gnome-extensions.nix
+++ modules/nixosModules/gnome-extensions.nix
@@ -1,54 +1,65 @@
-{pkgs, ...}: let
-  # --- Configuration Variables ---
-  # UUIDs found in: https://extensions.gnome.org/ or by running `gnome-extensions list`
-  batteryUuid = "battery-health-charging@maniacx.github.com";
-  windowCallsUuid = "window-calls@swyknox.github.com";
-in {
-  # 1. Install the Extensions
+{ config, pkgs, lib, ... }:
+
+let
+  # Define the extension package once to ensure paths match exactly
+  batteryExtension = pkgs.gnomeExtensions.battery-health-charging;
+  
+  # The helper script path inside the extension
+  # Note: The UUID must match exactly what is in the /share path of the package
+  extensionUuid = "battery-health-charging@maniacx.github.com";
+  executablePath = "${batteryExtension}/share/gnome-shell/extensions/${extensionUuid}/resources/batteryhealthchargingctl";
+
+  # Define the Polkit Policy strictly matching the installer's XML template
+  batteryHealthPolkit = pkgs.writeTextFile {
+    name = "battery-health-charging-polkit";
+    destination = "/share/polkit-1/actions/org.freedesktop.policykit.batteryhealthcharging.setthreshold.policy";
+    text = ''
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE policyconfig PUBLIC "-//freedesktop//DTD PolicyKit Policy Configuration 1.0//EN" "http://www.freedesktop.org/standards/PolicyKit/1/policyconfig.dtd">
+      <policyconfig>
+        <vendor>Battery Health Charging</vendor>
+        <vendor_url>https://github.com/maniacx/Battery-Health-Charging</vendor_url>
+        
+        <action id="org.freedesktop.policykit.batteryhealthcharging.setthreshold">
+          <description>Control battery thresholds</description>
+          <message>No Authorization required to control battery thresholds.</message>
+          <defaults>
+            <allow_any>yes</allow_any>
+            <allow_inactive>yes</allow_inactive>
+            <allow_active>yes</allow_active>
+          </defaults>
+          
+          <annotate key="org.freedesktop.policykit.exec.path">${executablePath}</annotate>
+          
+          <annotate key="org.freedesktop.policykit.batteryhealthcharging.setthreshold.polkit-rule.version">3.0.0</annotate>
+        </action>
+      </policyconfig>
+    '';
+  };
+
+in
+{
+  # 1. Install Extensions + The Polkit Rule
   environment.systemPackages = with pkgs; [
-    gnomeExtensions.battery-health-charging
+    batteryExtension
     gnomeExtensions.window-calls
-    (pkgs.writeTextFile {
-      name = "battery-health-charging-policy";
-      destination = "/share/polkit-1/actions/org.freedesktop.policykit.batteryhealthcharging.setthreshold.policy";
-      text = ''
-        <?xml version="1.0" encoding="UTF-8"?>
-        <!DOCTYPE policyconfig PUBLIC "-//freedesktop//DTD PolicyKit Policy Configuration 1.0//EN" "http://www.freedesktop.org/standards/PolicyKit/1/policyconfig.dtd">
-        <policyconfig>
-          <vendor>Battery Health Charging</vendor>
-          <vendor_url>https://github.com/maniacx/Battery-Health-Charging</vendor_url>
-          <action id="org.freedesktop.policykit.batteryhealthcharging.setthreshold">
-            <description>Control battery thresholds</description>
-            <message>No Authorization required to control battery thresholds.</message>
-            <defaults>
-              <allow_any>yes</allow_any>
-              <allow_inactive>yes</allow_inactive>
-              <allow_active>yes</allow_active>
-            </defaults>
-            <annotate key="org.freedesktop.policykit.exec.path">${pkgs.gnomeExtensions.battery-health-charging}/share/gnome-shell/extensions/${batteryUuid}/resources/batteryhealthchargingctl</annotate>
-            <annotate key="org.freedesktop.policykit.batteryhealthcharging.polkit-rule.version">3.0.0</annotate>
-          </action>
-        </policyconfig>
-      '';
-    })
+    batteryHealthPolkit
+    # Add other extensions here
   ];
 
-  # 3. Automatically Enable Extensions (Dconf)
-  # This avoids you having to manually toggle them on in the "Extensions" app.
-  programs.dconf.profiles.user.databases = [
-    {
-      settings = {
-        "org/gnome/shell" = {
-          disable-user-extensions = false;
-          enabled-extensions = [
-            batteryUuid
-            windowCallsUuid
-          ];
-        };
+  # 2. Enable extensions by default (Dconf)
+  programs.dconf.profiles.user.databases = [{
+    settings = {
+      "org/gnome/shell" = {
+        disable-user-extensions = false;
+        enabled-extensions = [
+          extensionUuid
+          "window-calls@swyknox.github.com"
+        ];
       };
-    }
-  ];
+    };
+  }];
 
-  # Ensure Polkit is active (usually default, but good to be explicit)
+  # 3. Ensure Polkit is enabled
   security.polkit.enable = true;
 }
