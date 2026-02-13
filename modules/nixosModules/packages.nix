a62e0eabd3eb4135bdd5c56120e25d40a47fbda8
murlakatam <baranovsky.e@gmail.com>
2026-02-13 15:16:22 +1100

    NixOS Gen 10 (26.05.20260208.d6c7193)

--- modules/nixosModules/packages.nix
+++ modules/nixosModules/packages.nix
@@ -13,6 +13,7 @@
 
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     #  wget
+    age # modern key gen
     alejandra
     azure-artifacts-credprovider
     azure-cli
