ea964961cec5279e772cbb2d263bc5572b246dd7
murlakatam <baranovsky.e@gmail.com>
2026-07-05 22:19:23 +1000

    NixOS Gen 20 (26.05.20260306.aca4d95)

--- modules/nixosModules/nix.nix
+++ modules/nixosModules/nix.nix
@@ -10,4 +10,9 @@
       options = "--delete-older-than 7d";
     };
   };
+
+  systemd.targets.sleep.enable = false;
+  systemd.targets.suspend.enable = false;
+  systemd.targets.hibernate.enable = false;
+  systemd.targets.hybrid-sleep.enable = false;
 }
